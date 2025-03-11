6dfa856b10338fa7b1231abddbfb1b5a157a297f
qing-xin-era <920685516@qq.com>
2025-03-11 20:59:30 +0800

    第0007次提交

---
+++ src/main/java/com/qingxing/train02/demos/web/Train02Application9.java
@@ -0,0 +1,13 @@
+package com.qingxing.train02.demos.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Train02Application9 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Train02Application9.class, args);
+    }
+
+}
