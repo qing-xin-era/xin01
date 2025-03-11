3c586a9569e309b80de6a25c5a20890408d8cc87
qing-xin-era <920685516@qq.com>
2025-03-11 21:47:26 +0800

    Revert "第0007次提交"
    This reverts commit 62be32a2

--- src/main/java/com/qingxing/train02/demos/web/Train02Application20.java
+++ src/main/java/com/qingxing/train02/demos/web/Train02Application20.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Train02Application11 {
+public class Train02Application20 {
 
     public static void main(String[] args) {
-        SpringApplication.run(Train02Application11.class, args);
+        SpringApplication.run(Train02Application20.class, args);
     }
 
 }
